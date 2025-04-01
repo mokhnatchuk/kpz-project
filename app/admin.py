abd686e52af1aea6b0a796bc6a89153290105d2a
Vasyl <144223353+mokhnatchuk@users.noreply.github.com>
2025-04-01 06:22:40 +0000

    Create models and work with ORM

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+    fieldsets = (
+        ('', {
+            'fields': ('category',),
+        }),
+    )
+
+admin.site.register(Category, CategoryAdmin)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField('Категорія', max_length=250, help_text='Максимум 250 символів')
+    slug = models.SlugField('Слаг', unique=True, help_text='Automatically generated slug')  # New field
+
+    class Meta:
+        verbose_name = 'Категорія для публікації'
+        verbose_name_plural = 'Категорії для публікацій'
+
+    def __str__(self):
+        return self.category
