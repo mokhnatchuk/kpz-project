9af616ffff33681931e07f8fece5570eee0301e6
Vasyl <144223353+mokhnatchuk@users.noreply.github.com>
2025-04-04 11:22:53 +0000

    Update admin and model

--- app/admin.py
+++ app/admin.py
@@ -1,12 +1,50 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Article, ArticleImage
+from django.shortcuts import get_object_or_404
+from .forms import ArticleImageForm
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('category',)
+    list_display = ('category', 'slug')
+    prepopulated_fields = {'slug': ('category',)}
     fieldsets = (
         ('', {
-            'fields': ('category',),
+            'fields': ('category', 'slug'),
         }),
     )
 
 admin.site.register(Category, CategoryAdmin)
+
+
+class ArticleImageInline(admin.TabularInline):
+    model = ArticleImage
+    form = ArticleImageForm
+    extra = 0
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'image',),
+        }),
+    )
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page')
+    inlines = [ArticleImageInline]
+    multiupload_form = True
+    multiupload_list = False
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('category',)
+    fieldsets = (
+        ('', {
+            'fields': ('pub_date', 'title', 'description', 'main_page', 'category'),
+        }),
+        (('Додатково'), {
+            'classes': ('collapse',),
+            'fields': ('slug',),
+        }),
+    )
+
+    def delete_file(self, pk, request):
+        obj = get_object_or_404(ArticleImage, pk=pk)
+        return obj.delete()
+
+admin.site.register(Article, ArticleAdmin)
