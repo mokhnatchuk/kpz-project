e9c20ef27eb94019c98fc9cc492960795063a307
Vasyl Mokhnatchuk <144223353+mokhnatchuk@users.noreply.github.com>
2025-04-15 11:06:28 +0300

    Fix AppRegistryNotReady error

--- app/models.py
+++ app/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.utils import timezone
-from django.urls import reverse
 
 class Category(models.Model):
     category = models.CharField('Категорія', max_length=250, help_text='Максимум 250 символів')
     slug = models.SlugField('Слаг', unique=True, help_text='Automatically generated slug')
 
     class Meta:
-        verbose_name = 'Категорія для публікації'
-        verbose_name_plural = 'Категорії для публікацій'
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
 
     def __str__(self):
         return self.category
 
+    def get_absolute_url(self):
+        return f"/articles/category/{self.slug}"
 
 class Article(models.Model):
     title = models.CharField('Заголовок', max_length=250, help_text='Максимум 250 сим.')
@@ -21,8 +22,6 @@ class Article(models.Model):
     slug = models.SlugField('Слаг', unique_for_date='pub_date')
     main_page = models.BooleanField('Головна', default=True, help_text='Показувати на головній сторінці')
     category = models.ForeignKey(Category, related_name='articles', blank=True, null=True, verbose_name='Категорія', on_delete=models.CASCADE)
-
-    objects = models.Manager()
 
     class Meta:
         ordering = ['-pub_date']
@@ -33,17 +32,7 @@ class Article(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        try:
-            url = reverse('news-detail', kwargs={
-                'year': self.pub_date.strftime("%Y"),
-                'month': self.pub_date.strftime("%m"),
-                'day': self.pub_date.strftime("%d"),
-                'slug': self.slug,
-            })
-        except:
-            url = "/"
-        return url
-
+        return f"/articles/{self.pub_date.strftime('%Y')}/{self.pub_date.strftime('%m')}/{self.pub_date.strftime('%d')}/{self.slug}"
 
 class ArticleImage(models.Model):
     article = models.ForeignKey(Article, verbose_name='Стаття', related_name='images', on_delete=models.CASCADE)
@@ -56,7 +45,3 @@ class ArticleImage(models.Model):
 
     def __str__(self):
         return self.title
-
-    @property
-    def filename(self):
-        return self.image.name.rsplit('/', 1)[-1]
