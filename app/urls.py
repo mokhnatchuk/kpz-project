5d8ca2a843a08ac6bb5b513ea5362a5918bb5fc9
Vasyl Mokhnatchuk <144223353+mokhnatchuk@users.noreply.github.com>
2025-04-15 11:13:23 +0300

    Fix URLs, simplify views

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, ArticleDetail, ArticleList, ArticleCategoryList
 
 urlpatterns = [
-    path("", views.app, name="app"),
+    path('', HomePageView.as_view(), name='home'),
+    path('articles', ArticleList.as_view(), name='articles-list'),
+    path('articles/category/<slug>', ArticleCategoryList.as_view(), name='articles-category-list'),
+    path('articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name='news-detail'),
 ]
